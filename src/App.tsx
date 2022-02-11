60f6ded6e4259abd871a911a9951bad4e1521bb3
bennyhsieh <benny.hsieh@jyintgroup.com>
2022-02-11 14:55:59 +0800

    feat: remove emotion and react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
 import React from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
 import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from '@emotion/react';
-import { CssBaseline } from '@mui/material';
 
 import AppRoutes from './AppRoutes';
 import Layout from './components/layout/Layout';
-import theme from './theme';
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-});
 
 const App: React.FC = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Layout>
-            <AppRoutes />
-          </Layout>
-        </ThemeProvider>
-      </BrowserRouter>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <BrowserRouter>
+      <Layout>
+        <AppRoutes />
+      </Layout>
+    </BrowserRouter>
   );
 };
 
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
-import { Box, Container } from '@mui/material';
 
 import Footer from './Footer';
 
 const Layout: React.FC = ({ children }) => {
   return (
-    <Box
-      id="app_container"
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        minHeight: '100vh',
-      }}
-    >
-      <Container sx={{ flexGrow: 1 }}>{children}</Container>
+    <div id="app_container">
+      <div>{children}</div>
       <Footer />
-    </Box>
+    </div>
   );
 };
 
